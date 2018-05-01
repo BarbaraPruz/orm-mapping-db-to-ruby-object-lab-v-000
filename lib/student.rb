5a5c93d74cc63ce3b98df31fe8386fd80eb668e2
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-01 22:35:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,14 +76,12 @@ class Student
   end
 
   def self.first_X_students_in_grade_10 (n)
-    students = []
     sql = <<-SQL
       SELECT * from students
       WHERE grade=10
       LIMIT ?
       SQL
-    DB[:conn].execute(sql,n).each { | row | students.push(self.new_from_db(row))}
-    students
+    DB[:conn].execute(sql,n).map { | row | self.new_from_db(row)}
   end
 
   def self.first_student_in_grade_10
